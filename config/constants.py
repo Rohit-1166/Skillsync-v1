a4c4be85da21c9494239e99e71ed55a4df103c74
Rohit-1166 <rohit.kumar24b@iiitg.ac.in>
2026-06-12 14:40:00 +0530

    Define supported categories and requirements versions

---
+++ config/constants.py
@@ -0,0 +1,28 @@
+SUPPORTED_WORK_MODES = {
+    "remote",
+    "onsite",
+    "hybrid",
+    "flexible"
+}
+
+SUPPORTED_DEGREES = {
+    "B.Tech",
+    "B.E.",
+    "M.Tech",
+    "M.E.",
+    "M.S.",
+    "PhD"
+}
+
+SENIORITY_LEVELS = {
+    "intern",
+    "junior",
+    "associate",
+    "engineer",
+    "senior",
+    "staff",
+    "principal",
+    "lead",
+    "manager",
+    "director"
+}
